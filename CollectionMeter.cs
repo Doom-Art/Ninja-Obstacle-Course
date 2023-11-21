8630fde656311294965303f0a92b6f8e6d08105e
Doom Art <Ouginak121@gmail.com>
2023-11-20 23:59:36 -0500

    minor changes

---
+++ CollectionMeter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninja_Obstacle_Course
+{
+    public class CollectionMeter
+    {
+        Texture2D texture;
+        Rectangle location;
+        float timer;
+        Color color;
+    }
+}
--- Environment.cs
+++ Environment.cs
@@ -6,14 +6,26 @@ namespace Ninja_Obstacle_Course
 {
     public class Environment
     {
-        private Texture2D _backgroundTex;
+        private Texture2D _backgroundTex, _collectibleTex;
         private Rectangle _startPosition;
+        private bool _hasCollectible;
+        private CollectionMeter _meter;
         public Environment(Texture2D backgroundTex, Rectangle startLoc)
         {
             _backgroundTex = backgroundTex;
             _startPosition = startLoc;
             MaxGravity = 9.8f;
         }
+        public Environment(Texture2D backgroundTex, Texture2D collectibleTex, Rectangle startPosition, bool hasCollectible, CollectionMeter collector)
+        {
+            _backgroundTex = backgroundTex;
+            _collectibleTex = collectibleTex;
+            _startPosition = startPosition;
+            _hasCollectible = hasCollectible;
+            _meter = collector;
+            MaxGravity = 9.8f;
+        }
+
         public float MaxGravity
         {
             get; set;
